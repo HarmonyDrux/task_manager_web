9cdc7657b8757a5f763925b6944c5d9e80f46f3e
HarmonyDrux <beatrizpessanha87@gmail.com>
2026-01-17 15:45:38 -0300

    added a font and a font size.
    added connections with todos.txt so the user can add a todo.
    
    added sg.WINDOW_CLOSED to prevent crash shutdown on GUI

--- Phyton Mega Course/Task Manager (TO-DO)/gui.py
+++ Phyton Mega Course/Task Manager (TO-DO)/gui.py
@@ -3,9 +3,25 @@ import FreeSimpleGUI as sg
 import emoji
 
 label = sg.Text(f"Type an TO-DO {emoji.emojize(':thinking_face:')}")
-input_box = sg.InputText(tooltip='Enter todo')
+input_box = sg.InputText(tooltip='Enter todo', key="todo")
 add_button = sg.Button('Add')
 
-window = sg.Window(f'TO-DO {emoji.emojize(':memo:')} App', layout=[[label], [input_box, add_button]])
-window.read()
+window = sg.Window(f'TO-DO {emoji.emojize(':memo:')} App',
+                   layout=[[label], [input_box, add_button]],
+                   font=("Helvetica", 20))
+
+while True:
+    event, values = window.read() #Mostra tudo o que foi executado pelo usuário no gui.
+    # Ex: event: Add pois o usuário clicou em add,
+    # values = {'todo': 'clean bathroom'} pois o usuario digitou clean bathroom
+    match event:
+        case "Add":
+            todos = functions.read_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WINDOW_CLOSED:
+            '''fecha o GUI prevenindo que tenha erro na GUI'''
+            break
+
 window.close()
