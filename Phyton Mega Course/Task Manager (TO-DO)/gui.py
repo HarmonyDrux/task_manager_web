9aa6d1df801f77b13d621ae060bc13b01cebeb5d
HarmonyDrux <beatrizpessanha87@gmail.com>
2026-01-17 18:03:55 -0300

    added a size to the gui
    added edit and a connectio with the todos.txt
    
    now we can see list edited in real time and we can see the option we want to edit in the input box

--- Phyton Mega Course/Task Manager (TO-DO)/gui.py
+++ Phyton Mega Course/Task Manager (TO-DO)/gui.py
@@ -5,9 +5,12 @@ import emoji
 label = sg.Text(f"Type an TO-DO {emoji.emojize(':thinking_face:')}")
 input_box = sg.InputText(tooltip='Enter todo', key="todo")
 add_button = sg.Button('Add')
+list_box = sg.Listbox(values=functions.read_todos(), key="todos",
+                      enable_events=True, size=(45,10))
+edit_button = sg.Button('Edit')
 
 window = sg.Window(f'TO-DO {emoji.emojize(':memo:')} App',
-                   layout=[[label], [input_box, add_button]],
+                   layout=[[label], [input_box, add_button],[list_box, edit_button]],
                    font=("Helvetica", 20))
 
 while True:
@@ -18,8 +21,20 @@ while True:
         case "Add":
             todos = functions.read_todos()
             new_todo = values['todo'] + "\n"
-            todos.append(new_todo)
+            todos.append(new_todo.capitalize())
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo'] + "\n"
+
+            todos = functions.read_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo.capitalize()
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "todos":
+            window['todo'].update(value=values['todos'][0])
         case sg.WINDOW_CLOSED:
             '''fecha o GUI prevenindo que tenha erro na GUI'''
             break
